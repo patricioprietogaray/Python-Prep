c295be6c9128e54f2e779482d679f01144744285
patricioprietogaray <patricioprietogaray@gmail.com>
2026-07-22 20:26:24 -0300

    modulo 3 completo

--- M02_introprogramación/Practica.py
+++ M02_introprogramación/Practica.py
@@ -1,7 +1,8 @@
 
 # Este es un archivo de prueba, sin actividades =) .
 
-# A partir del próximo módulo: M03_variablesydatos vas a tener actividades para realizar.
+# A partir del próximo módulo: M03_variablesydatos 
+# vas a tener actividades para realizar.
 
 print('Hola Mundo!')
 a = 20
